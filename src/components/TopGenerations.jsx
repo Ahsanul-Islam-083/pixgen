536198cb05699a20e3f1c2986f814a0c964af7cf
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-05-01 16:25:36 +0600

    feat: update grid layout in TopGenerations for improved responsiveness

--- src/components/TopGenerations.jsx
+++ src/components/TopGenerations.jsx
@@ -11,7 +11,7 @@ const TopGenerations = async() => {
     return (
         <div>
             <h1 className="text-2xl font-bold my-5">Top Generations</h1>
-            <div className="grid grid-cols-4 gap-5">
+            <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-5">
                 {
                     topPhotos.map(photo=> <PhotoCard key={photo.id} photo={photo} />)
                 }
